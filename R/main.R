487fada0aeba1b2c2657d3f5edd1277acdab9c3b
Spencer Schien <spencer.schien@gmail.com>
2022-12-18 17:53:44 -0600

    commented script

--- R/main.R
+++ R/main.R
@@ -1,7 +1,12 @@
+# install data with install.packages("palmerpenguins")
+
 library(palmerpenguins)
 library(tidyverse)
 
 glimpse(penguins)
+
+# transform data for our desired format that will
+# facilitate the making of our plot
 
 long <- penguins |> 
   select(species,
@@ -16,7 +21,12 @@ long <- penguins |>
   summarise(avg_length = mean(length, na.rm = TRUE)) |> 
   filter(!is.na(sex))
 
+# make plot
+
 long |> 
+  
+  # mutate data so labels are how we want them
+  
   mutate(measurement = ifelse(measurement == "bill_depth_mm",
                               "Avg. Bill\nDepth",
                               "Avg. Bill\nLength"),
@@ -24,13 +34,29 @@ long |>
   ggplot(aes(measurement, avg_length, fill = sex)) +
   geom_col(position = "dodge",
            width = .75) +
+  
+  # expand will add space on the ends of the axis
+  
   scale_x_discrete(expand = c(.5, .5)) +
+  
+  # manually set the fill colors
+  
   scale_fill_manual(values = c("darkorange", "cyan4")) +
+  
+  # strip.position moves the facet title
+  
   facet_wrap(~ species, strip.position = "bottom") +
   theme_minimal() +
+  
   theme(panel.grid.minor = element_blank(),
         panel.grid.major.x = element_blank(),
+        
+        # move stip text below x axis labels
+        
         strip.placement = "outside",
+        
+        # smash panels together
+        
         panel.spacing = unit(0, "cm"),
         legend.position = "bottom",
         strip.text = element_text(size = 20, face = "bold"),
@@ -48,5 +74,7 @@ long |>
        caption = "Data from {palmerpenquins} R package.",
        title = "Comparative Bill Sizes",
        subtitle = "Dimensions for male and female Adelie, Chinstrap, and Gentoo penguins at Palmer Station LTER.")
+
+# save plot
 
 ggsave("plots/final_plot.png", bg = "white")
